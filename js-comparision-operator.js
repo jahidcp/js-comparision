00b8d5bcc744892689fc4fed3efda6d700a6d05b
jahidcp <programmingloverjahid@gmail.com>
2023-01-27 19:40:19 +0600

    js comparision operator practice

--- js-comparision-operator.js
+++ js-comparision-operator.js
@@ -1,21 +1,21 @@
 var num1 = 456;
 var num2 = 256;
-console.log(num1 > num2)
-console.log(num2 < num1)
-console.log(num2 > num1)
-console.log(num1 == num2)
-console.log(num1 != num2)
+console.log(num1 > num2);
+console.log(num2 < num1);
+console.log(num2 > num1);
+console.log(num1 == num2);
+console.log(num1 != num2);
 
 // Less then or equal
-console.log(num1 <= num2)
-console.log(num2 <= num1)
+console.log(num1 <= num2);
+console.log(num2 <= num1);
 
 // greater then or equal
 
-console.log(num1 >= num2)
+console.log(num1 >= num2);
 
-console.log(num2 >= num1)
+console.log(num2 >= num1);
 
-console.log(num1 > num2 && num1 < num2)
+console.log(num1 > num2 && num1 < num2);
 
-console.log(num1 >= num2 || num1 <= num2)
+console.log(num1 >= num2 || num1 <= num2);
